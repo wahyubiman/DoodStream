f4cac83d2400506e2833292ea592e5cd69ddf03d
Ra <108156134+rainrisa@users.noreply.github.com>
2022-08-01 07:41:16 +0700

    Fix IndexError: list index out of range

--- doodstream/api.py
+++ doodstream/api.py
@@ -1,6 +1,7 @@
 import requests
 import re
 import sys
+import json
 
 class DoodStream:
     '''Python doodstream api wrapper from official https://doodstream.com/api
@@ -56,10 +57,9 @@ class DoodStream:
         filename = path.split("/")[-1]
         post_files = {"file": (filename, open(path, "rb"))}
         up = requests.post(url_for_upload, data=post_data, files=post_files)
-        st = re.findall(r'name="st">(.*?)</text' , str(up.text))
-        fn = re.findall(r'name="fn">(.*?)</text' , str(up.text))
-        if st[0] == "OK":
-            return {"status": st[0], "file_id": fn[0], "file_url": f"https://doodstream.com/d/{fn[0]}"}
+        res = json.loads(up.text)
+        if res["msg"] == "OK":
+            return res
         else:
             raise TypeError(f"unsupported video format {filename}, please upload video with mkv, mp4, wmv, avi, mpeg4, mpegps, flv, 3gp, webm, mov, mpg & m4v format")
         
